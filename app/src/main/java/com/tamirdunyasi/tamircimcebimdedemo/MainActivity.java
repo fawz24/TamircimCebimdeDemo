05e531c6760a8b86525591c3866e37bba6f77f4e
fawzy abderrahman <fawzyabderrahman@gmail.com>
2018-12-04 00:00:17 +0300

    MainActivity generation

--- app/src/main/java/com/tamirdunyasi/tamircimcebimdedemo/MainActivity.java
+++ app/src/main/java/com/tamirdunyasi/tamircimcebimdedemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tamirdunyasi.tamircimcebimdedemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.main_activity_label);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void goToLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToRegister(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/tamirdunyasi/tamircimcebimdedemo/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.tamirdunyasi.tamircimcebimdedemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setTitle(R.string.main_activity_label);
+    }
+}
